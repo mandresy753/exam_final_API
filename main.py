31643eef7909ae7ec94cec30ac3de7316238c487
RANDRIANANTOANINA Mandresy Gaëtan <mandresy2020@gmail.com>
2025-09-03 09:37:11 +0300

    add: response of exo1 in main.py

--- main.py
+++ main.py
@@ -1,8 +1,44 @@
-from fastapi import FastAPI
-from starlette.responses import JSONResponse
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import List, Optional
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return JSONResponse(content={"message": "Hello FastAPI !"})
+class Characteristics(BaseModel):
+    ram_memory: int
+    rom_memory: int
+
+
+class Phone(BaseModel):
+    identifier: str
+    model: str
+    characteristics: Characteristics
+
+phones = []
+
+@app.get("/health")
+def health_check():
+    return "Ok"
+
+@app.post("/phones", status_code=201)
+async def create_phone(phone: Phone):
+    phones.append(phone)
+    return phone
+
+@app.get("/phones", response_model=List[Phone])
+async def get_phones():
+    return phones
+
+@app.get("/phones/{id}")
+async def get_phone(id: str):
+    for phone in phones:
+        if phone.identifier == id:
+            return phone
+    raise HTTPException(status_code=404, detail="telephone non trouvé")
+
+@app.put("/phones/{id}/characteristics")
+async def update_characteristics(id: str, characteristics: Characteristics):
+    for phone in phones:
+        if phone.identifier == id:
+            phone.characteristics = characteristics
+            return {"message": "Characteristiques mis a jour succes"}
